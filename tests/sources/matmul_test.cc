2f3c26dbfe9162ded73601656715ac3e4d4219f1
Lazar Djurovic <ldjurovic@tenstorrent.com>
2024-10-31 15:21:22 +0000

    Working on matmul test; sometimes passing

---
+++ tests/sources/matmul_test.cc
@@ -0,0 +1,80 @@
+
+/*
+===================================
+All constants are defined in file ../helper/params.h
+and are set during compilation.
+
+DATA_FORMAT -> used insinde UNPACK and PACK
+ELTWISE_BINARY_OP -> used inside of MATH for binary eltwise kernels 
+
+===================================
+*/
+
+#include <cstdint>
+#include <cstdio>
+
+#include "llk_defs.h"
+#include "ckernel.h"
+
+// Globals
+uint32_t unp_cfg_context = 0;
+uint32_t pack_sync_tile_dst_ptr = 0;
+volatile uint32_t tt_l1_ptr l1_buffer[16] __attribute__ ((section (".text#"))) __attribute__ ((aligned (16)));
+
+#ifdef LLK_TRISC_UNPACK
+
+#include "llk_unpack_AB_matmul.h"
+#include "../helpers/params.h"
+
+volatile uint32_t* buffer_A = (volatile uint32_t*)0x1b000;
+volatile uint32_t* buffer_B = (volatile uint32_t*)0x1c000;
+
+void run_kernel()
+{
+    _llk_unpack_AB_matmul_hw_configure_<>(DATA_FORMAT, DATA_FORMAT, DATA_FORMAT, DATA_FORMAT);
+    _llk_unpack_AB_matmul_init_<>();
+    _llk_unpack_AB_matmul_<>((std::uint32_t)buffer_A/16-1,(std::uint32_t)buffer_B/16-1,0,0,1,1);
+}
+
+#endif
+
+#ifdef LLK_TRISC_MATH
+
+#include "llk_math_common.h"
+#include "llk_math_matmul.h"
+#include "../helpers/params.h"
+
+void run_kernel()
+{
+    _llk_math_matmul_init_<4,DstTileFaceLayout::RowMajor>();
+    _llk_math_pack_sync_init_<DstSync::SyncFull,false>();
+    _llk_math_hw_configure_<false,false>(DATA_FORMAT,DATA_FORMAT);
+    _llk_math_wait_for_dest_available_<DstSync::SyncFull>();
+    _llk_math_matmul_<4,DstTileFaceLayout::RowMajor>(0,false,1,1,1);
+    _llk_math_dest_section_done_<DstSync::SyncFull,false>();
+}
+
+#endif 
+
+#ifdef LLK_TRISC_PACK
+
+#include "llk_pack.h"
+#include "llk_pack_common.h"
+#include "../helpers/params.h"
+
+volatile uint32_t* buffer_Dest = (volatile uint32_t*)0x1a000;
+void run_kernel()
+{
+    for(int i = 0; i < 16*16*4; i++)
+    {
+        buffer_Dest[i] = 0xdeadbeef;
+    }
+    _llk_pack_hw_configure_(DATA_FORMAT, DATA_FORMAT, 16*16*4);
+    _llk_pack_init_<false, false, DstTileFaceLayout::RowMajor, false>(DATA_FORMAT);
+    _llk_pack_dest_init_<DstSync::SyncFull, DstTileFaceLayout::RowMajor, false, false>();
+    _llk_packer_wait_for_math_done_();
+    _llk_pack_<DstSync::SyncFull>(0, (std::uint32_t)buffer_Dest/16-1);
+    _llk_pack_dest_section_done_<DstSync::SyncFull,false>();
+}
+
+#endif
---
+++ tests/test_matmul.py
@@ -0,0 +1,94 @@
+import pytest
+import torch
+import os
+from dbd.tt_debuda_init import init_debuda
+from dbd.tt_debuda_lib import write_to_device, read_words_from_device, run_elf
+from pack import *
+from unpack import *
+
+format_dict = {
+    "Float32": torch.float32,
+    "Float16": torch.float16,
+    "Float16_b": torch.bfloat16,
+    "Int32": torch.int32
+}
+
+format_args_dict = {
+    "Float32": "FORMAT_FLOAT32",
+    "Float16": "FORMAT_FLOAT16",
+    "Float16_b": "FORMAT_FLOAT16_B",
+    "Int32": "FORMAT_INT32"
+}
+
+mathop_args_dict = {
+    "elwadd": "ELTWISE_BINARY_ADD",
+    "elwsub": "ELTWISE_BINARY_SUB",
+    "elwmul": "ELTWISE_BINARY_MUL"
+}
+
+def generate_stimuli(stimuli_format):
+    # srcA = torch.rand(1024, dtype=format_dict[stimuli_format]) + 0.5
+    # srcB = torch.rand(1024, dtype=format_dict[stimuli_format]) + 0.5
+
+    srcA = torch.full((1024,), 2, dtype=format_dict[stimuli_format])
+    srcB = torch.full((1024,), 2, dtype=format_dict[stimuli_format])
+
+    return srcA, srcB
+
+def generate_golden(operand1, operand2, data_format):
+    A_float = operand1.clone().detach().to(format_dict[data_format])
+    B_float = operand2.clone().detach().to(format_dict[data_format])
+
+    A_reshaped = A_float.view(32, 32)
+    B_reshaped = B_float.view(32, 32)
+
+    result = torch.matmul(A_reshaped, B_reshaped)
+
+    result = result.view(-1)
+
+    return result.tolist()
+
+def write_stimuli_to_l1(buffer_A, buffer_B, stimuli_format):
+    if stimuli_format == "Float16_b":
+        write_to_device("18-18", 0x1b000, pack_bfp16(buffer_A))
+        write_to_device("18-18", 0x1c000, pack_bfp16(buffer_B))    
+    elif stimuli_format == "Float16":
+        write_to_device("18-18", 0x1b000, pack_fp16(buffer_A))
+        write_to_device("18-18", 0x1c000, pack_fp16(buffer_B))
+
+@pytest.mark.parametrize("format", ["Float16_b"])  # , "Float16"])
+@pytest.mark.parametrize("testname", ["matmul_test"])
+@pytest.mark.parametrize("machine", ["wormhole"])
+def test_all(format, testname, machine):
+    context = init_debuda()
+    src_A, src_B = generate_stimuli(format)
+    golden = generate_golden(src_A, src_B, format)
+    write_stimuli_to_l1(src_A, src_B, format)
+
+    make_cmd = f"make format={format_args_dict[format]} testname={testname} machine={machine}"
+    os.system(make_cmd)
+
+    for i in range(3):
+        run_elf(f"build/elf/{testname}_trisc{i}.elf", "18-18", risc_id=i + 1)
+
+    read_words_cnt = len(src_A) // (2 if format in ["Float16", "Float16_b"] else 1)
+    read_data = read_words_from_device("18-18", 0x1a000, word_count=read_words_cnt)
+    
+    read_data_bytes = flatten_list([int_to_bytes_list(data) for data in read_data])
+    
+    res_from_L1 = unpack_bfp16(read_data_bytes) if format == "Float16_b" else unpack_fp16(read_data_bytes)
+
+    assert len(res_from_L1) == len(golden)
+
+    os.system("make clean")
+
+    # Mailbox checks
+    assert read_words_from_device("18-18", 0x19FF4, word_count=1)[0].to_bytes(4, 'big') == b'\x00\x00\x00\x01'
+    assert read_words_from_device("18-18", 0x19FF8, word_count=1)[0].to_bytes(4, 'big') == b'\x00\x00\x00\x01'
+    assert read_words_from_device("18-18", 0x19FFC, word_count=1)[0].to_bytes(4, 'big') == b'\x00\x00\x00\x01'
+
+    tolerance = 0.1
+    for i in range(len(golden)):
+        read_word = hex(read_words_from_device("18-18", 0x1a000 + (i // 2) * 4, word_count=1)[0])
+        if golden[i] != 0:
+            assert abs((res_from_L1[i] - golden[i]) / golden[i]) <= tolerance, f"i = {i}, {golden[i]}, {res_from_L1[i]} {read_word}"
