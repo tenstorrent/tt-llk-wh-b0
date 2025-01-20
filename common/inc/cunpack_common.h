d14036c738793c53f92d83625dcd463a24a0ac3d
adjordjevic-TT <adjordjevic@tenstorrent.com>
2025-01-20 12:54:13 +0000

    Changed readers for config structs to return array of config struct instances

--- common/inc/cunpack_common.h
+++ common/inc/cunpack_common.h
@@ -8,6 +8,8 @@
 #include "ckernel.h"
 #include "ckernel_globals.h"
 
+#include <array>
+
 #ifdef PERF_DUMP
 #include "perf_res_decouple.h"
 #endif
@@ -452,28 +454,48 @@ namespace ckernel::unpacker
 
    // READERS FOR STRUCTS
 
-   inline unpack_tile_descriptor_t read_unpack_tile_descriptor() {
-      unpack_tile_descriptor_u tile_descriptor;
-      volatile uint tt_reg_ptr *cfg = get_cfg_pointer();
+   inline unpack_tile_descriptor_t read_unpack_tile_descriptor_helper(uint32_t reg_addr, const volatile uint tt_reg_ptr *cfg) {
+      unpack_tile_descriptor_u tile_descriptor = {.val = 0};
 
-      tile_descriptor.val[0] = cfg[THCON_SEC0_REG0_TileDescriptor_ADDR32];
-      tile_descriptor.val[1] = cfg[THCON_SEC0_REG0_TileDescriptor_ADDR32 + 1];
-      tile_descriptor.val[2] = cfg[THCON_SEC1_REG0_TileDescriptor_ADDR32];
-      tile_descriptor.val[3] = cfg[THCON_SEC1_REG0_TileDescriptor_ADDR32 + 1];
+      tile_descriptor.val[0] = cfg[reg_addr];
+      tile_descriptor.val[1] = cfg[reg_addr + 1];
+      tile_descriptor.val[2] = cfg[reg_addr + 2];
+      tile_descriptor.val[3] = cfg[reg_addr + 3];
       
       return tile_descriptor.f;
    }
 
-   inline unpack_config_t read_unpack_config() {
-      unpack_config_u config;
-      volatile uint tt_reg_ptr *cfg = get_cfg_pointer();
+   inline std::array<unpack_tile_descriptor_t, 2> read_unpack_tile_descriptor() {
+      std::array<unpack_tile_descriptor_t, 2> tile_descriptor_vec;
+      // Get pointer to registers for current state ID 
+      volatile uint tt_reg_ptr* cfg = get_cfg_pointer();
 
-      config.val[0] = cfg[THCON_SEC0_REG2_Out_data_format_ADDR32];
-      config.val[1] = cfg[THCON_SEC0_REG2_Out_data_format_ADDR32 + 1];
-      config.val[2] = cfg[THCON_SEC1_REG2_Out_data_format_ADDR32];
-      config.val[3] = cfg[THCON_SEC1_REG2_Out_data_format_ADDR32 + 1];
+      tile_descriptor_vec[0] = read_unpack_tile_descriptor_helper(THCON_SEC0_REG0_TileDescriptor_ADDR32, cfg);
+      tile_descriptor_vec[1] = read_unpack_tile_descriptor_helper(THCON_SEC1_REG0_TileDescriptor_ADDR32, cfg);
+
+      return tile_descriptor_vec;
+   }
+
+   inline unpack_config_t read_unpack_config_helper(uint32_t reg_addr, const volatile uint tt_reg_ptr *cfg) {
+      unpack_config_u config;
+
+      config.val[0] = cfg[reg_addr];
+      config.val[1] = cfg[reg_addr + 1];
+      config.val[2] = cfg[reg_addr + 2];
+      config.val[3] = cfg[reg_addr + 3];
 
       return config.f;
+   }
+
+   inline std::array<unpack_config_t, 2> read_unpack_config() {
+      std::array<unpack_config_t, 2> config_vec;
+      // Get pointer to registers for current state ID 
+      volatile uint tt_reg_ptr* cfg = get_cfg_pointer();
+
+      config_vec[0] = read_unpack_config_helper(THCON_SEC0_REG2_Out_data_format_ADDR32, cfg);
+      config_vec[1] = read_unpack_config_helper(THCON_SEC1_REG2_Out_data_format_ADDR32, cfg);
+
+      return config_vec;
    }
 
    inline alu_config_t read_alu_config() {
